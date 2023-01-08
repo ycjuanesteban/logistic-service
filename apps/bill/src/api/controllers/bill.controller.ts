76aa8c01b7e838b6769a86ab452a6cc7c8550807
Juan Estebanm <ycjuanesteban@gmail.com>
2023-01-08 20:03:09 +0100

    Stabilize

--- apps/bill/src/api/controllers/bill.controller.ts
+++ apps/bill/src/api/controllers/bill.controller.ts
@@ -1,12 +1,12 @@
 import { BaseController } from '@app/shared/api/controllers/base.controller';
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, Post } from '@nestjs/common';
 import { CommandBus } from '@nestjs/cqrs';
 import { ApiResponse, ApiTags } from '@nestjs/swagger';
 import { AddBillDto } from '../../application/dtos/addBillDto';
 import { BillFactoryService } from '../../application/factories/bill.factory.service';
 
 @ApiTags('Bill')
-@Controller('Bill')
+@Controller('/api/v1/bill')
 export class BillController extends BaseController {
   constructor(
     commandBus: CommandBus,
