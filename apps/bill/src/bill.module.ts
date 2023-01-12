a8fcdf7093c3070ed6b391366eddd8d071600f07
Juan Esteban <juan.esteban@barrabes.biz>
2023-01-12 18:02:50 +0100

    basic seeds

--- apps/bill/src/bill.module.ts
+++ apps/bill/src/bill.module.ts
@@ -1,9 +1,11 @@
-import { Module } from '@nestjs/common';
+import { Module, OnModuleInit } from '@nestjs/common';
 import { CqrsModule } from '@nestjs/cqrs';
+import { DataSource, Repository } from 'typeorm';
 import { BillController } from './api/controllers/bill.controller';
 import { commandsHandlers } from './application/extensions/providers.cqrs';
 import { factoriesServices } from './application/extensions/services';
 import { databaseProviders, repositoryProviders } from './data';
+import { Bill, BillDetail, Product, User } from './domain';
 
 @Module({
   imports: [CqrsModule],
@@ -15,4 +17,60 @@ import { databaseProviders, repositoryProviders } from './data';
     ...factoriesServices,
   ],
 })
-export class BillModule {}
+export class BillModule implements OnModuleInit {
+
+  async onModuleInit() {
+
+    let dbContext = await this.initializeDbContex();
+
+    await this.insertUsers(dbContext.getRepository(User));
+    await this.insertProducts(dbContext.getRepository(Product));
+  }
+
+  //Seeds methods
+  async insertUsers(userRepository: Repository<User>) {
+    if ((await userRepository.find()).length == 0) {
+      await userRepository.insert([
+        {
+          Name: 'User 1'
+        },
+        {
+          Name: 'User 2'
+        }
+      ])
+    }
+  }
+
+  async insertProducts(productRepository: Repository<Product>) {
+    if ((await productRepository.find()).length == 0) {
+      await productRepository.insert([
+        {
+          Name: 'Product 1'
+        },
+        {
+          Name: 'Product 2'
+        }
+      ])
+    }
+  }
+
+  //Initialize dbcontext
+  private initializeDbContex() {
+    return new DataSource({
+      type: "mysql",
+      entities: [
+        User,
+        Bill,
+        BillDetail,
+        Product
+      ],
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USER_NAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATA_BASE,
+      entityPrefix: 'bill_',
+    }).initialize();
+  }
+
+}
--- apps/logistic/src/logistic.module.ts
+++ apps/logistic/src/logistic.module.ts
@@ -1,8 +1,10 @@
-import { Module } from '@nestjs/common';
+import { Module, OnModuleInit } from '@nestjs/common';
 import { CqrsModule } from '@nestjs/cqrs';
+import { DataSource, Repository } from 'typeorm';
 import { ShippingController } from './api/controllers/shipping.controller';
 import { commandsHandlers } from './application/extensions/providers.cqrs';
 import { databaseProviders, repositoryProviders } from './data';
+import { BillDetailEntity, BillEntity, ProductEntity, ShippingEntity, UserEntity } from './data/models';
 
 @Module({
   imports: [CqrsModule],
@@ -13,4 +15,61 @@ import { databaseProviders, repositoryProviders } from './data';
     ...commandsHandlers
   ]
 })
-export class LogisticModule {}
+export class LogisticModule implements OnModuleInit {
+
+  async onModuleInit() {
+
+    let dbContext = await this.initializeDbContex();
+
+    await this.insertUsers(dbContext.getRepository(UserEntity));
+    await this.insertProducts(dbContext.getRepository(ProductEntity));
+  }
+
+  //Seeds methods
+  async insertUsers(userRepository: Repository<UserEntity>) {
+    if ((await userRepository.find()).length == 0) {
+      await userRepository.insert([
+        {
+          Name: 'User 1'
+        },
+        {
+          Name: 'User 2'
+        }
+      ])
+    }
+  }
+
+  async insertProducts(productRepository: Repository<ProductEntity>) {
+    if ((await productRepository.find()).length == 0) {
+      await productRepository.insert([
+        {
+          Name: 'Product 1'
+        },
+        {
+          Name: 'Product 2'
+        }
+      ])
+    }
+  }
+
+  //Initialize dbcontext
+  private initializeDbContex() {
+    return new DataSource({
+      type: "mysql",
+      entities: [
+        UserEntity,
+        BillEntity,
+        BillDetailEntity,
+        ProductEntity,
+        ShippingEntity
+      ],
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USER_NAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATA_BASE,
+      entityPrefix: 'logistic_',
+    }).initialize();
+  }
+
+}
